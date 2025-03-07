d24633a185f99bd88519f3674ed54fc1ad74c9e5
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-03-07 18:29:21 +0530

    Updating config

--- Go-worker/src/utils/config.go
+++ Go-worker/src/utils/config.go
@@ -4,14 +4,13 @@ import (
 	"errors"
 	"log"
 	"os"
-	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	ServerPort     int
 	GEMINI_API_KEY string
+	REDIS_URL      string
 }
 
 var AppConfig Config
@@ -23,22 +22,21 @@ func LoadEnv() (Config, error) {
 		return Config{}, err
 	}
 
-	portStr := os.Getenv("PORT")
-	port, err := strconv.Atoi(portStr)
-
-	if err != nil || port <= 0 {
-		port = 8900
-	}
-
 	GEMINI_API_KEY := os.Getenv("GEMINI_API_KEY")
 
 	if GEMINI_API_KEY == "" {
 		return Config{}, errors.New("GEMINI_API_KEY is required")
 	}
 
+	REDIS_URL := os.Getenv("REDIS_URL")
+
+	if REDIS_URL == "" {
+		return Config{}, errors.New("REDIS_URL is required")
+	}
+
 	AppConfig = Config{
-		ServerPort:     port,
 		GEMINI_API_KEY: GEMINI_API_KEY,
+		REDIS_URL:      REDIS_URL,
 	}
 
 	return AppConfig, nil
